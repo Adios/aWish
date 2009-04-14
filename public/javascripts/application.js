c29f20a2d0362be1329b7ecde45148e23d182d0e
Adios <adiosf6f@gmail.com>
2009-04-14 11:57:14 +0800

    adding ajax create operation.
     * import jquery.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var $j = jQuery.noConflict();
+
+function DesireList (jquery) {
+  this.list = jquery.get(0);
+}
+
+DesireList.prototype.insert = function(name) {
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(name));
+  
+  var price = document.createElement('div');
+  $j(price).addClass('price');
+  li.appendChild(price);
+  
+  this.list.insertBefore(li, $j('ul#desire > li:not(:first)').get(0));
+  return this;
+}
+
+$j(function() {
+   $j('ul#desire > li:first').hide();
+   
+   $j('a#create-button').click(function() {
+                               $j('ul#desire > li:first').show();
+                               $j('input#want_name').val('');
+                               return false;
+                               });
+})
