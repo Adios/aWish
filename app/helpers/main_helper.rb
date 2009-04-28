f3c61eee6c6363bdb90a756a2f8d7f04ecce2033
Adios <adiosf6f@gmail.com>
2009-04-28 17:11:29 +0800

    javascript refactory...
     * still needed to be improved...

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,7 +3,7 @@ module MainHelper
     case a
     when 'created_at', 'updated_at', 'note' then return ''
 	when 'name'
-	  return text_field_tag("desire[#{a}]", nil, :class => a)
+	  return text_field_tag("desire[#{a}]", nil, :class => a, :id => nil)
     else 
       return "<b>#{a}:</b> " + text_field_tag("desire[#{a}]", nil, :class => a)
     end
